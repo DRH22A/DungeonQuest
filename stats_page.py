50eedff379223727c1db9e391a297da397e4f65e
jackbrower <ejbrower4@gmail.com>
2024-04-22 21:14:50 -0400

    Fix stats_page to not reinit pygame

--- stats_page.py
+++ stats_page.py
@@ -1,16 +1,9 @@
 import pygame
-import sys
-import os
-
-from colorama import Fore, Back, Style
 
 import config
-from textbox import InputBox
-pygame.init()
 
 # Set up the display
-WIDTH, HEIGHT = 800, 600
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
+WIDTH, HEIGHT = config.WIDTH, config.HEIGHT
 pygame.display.set_caption("Player Stats")
 
 # Colors
@@ -18,16 +11,13 @@ WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 GRAY = (128, 128, 128)
 
-# Fonts
-font = pygame.font.SysFont(None, 40)
-
 def draw_text(screen, text: str, color: tuple, pos: tuple):
     font = pygame.font.Font("resources/PixelOperator8.ttf", 16)
     txt_surface = font.render(text, True, color)
     screen.blit(txt_surface, pos)
 
 
-def main_menu_button():
+def main_menu_button(screen):
     # Center the button horizontally, position it near the bottom of the screen
     button_rect = pygame.Rect((WIDTH - 200) // 2, HEIGHT - 100, 200, 50)
     pygame.draw.rect(screen, GRAY, button_rect)
@@ -35,8 +25,6 @@ def main_menu_button():
     return button_rect
 
 def show_stats_screen(screen):
-    width, height = config.WIDTH, config.HEIGHT
-
     # Dummy data for player stats
     total_score = 1000
     total_deaths = 5
@@ -60,6 +48,6 @@ def show_stats_screen(screen):
         draw_text(screen, "Total Runs: {}".format(total_runs), (255, 255, 255), (WIDTH // 2, HEIGHT // 2 - 50))
 
         # Draw the main menu button
-        main_menu_rect = main_menu_button()
+        main_menu_rect = main_menu_button(screen)
 
         pygame.display.flip()
