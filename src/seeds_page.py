c148aac9e9a7b02397f3bc31a8f0a3efa0482f52
evanrudd <evanrocksrudd@gmail.com>
2024-04-24 21:13:17 -0400

    seed inserting

--- src/seeds_page.py
+++ src/seeds_page.py
@@ -13,16 +13,19 @@ WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 GRAY = (128, 128, 128)
 
+
 def draw_text(screen, text: str, color: tuple, pos: tuple):
     font = pygame.font.Font("resources/PixelOperator8.ttf", 16)
     txt_surface = font.render(text, True, color)
     screen.blit(txt_surface, pos)
+
 
 def main_menu_button(screen):
     button_rect = pygame.Rect((WIDTH - 200) // 2, HEIGHT - 100, 200, 50)
     pygame.draw.rect(screen, GRAY, button_rect)
     draw_text(screen, "Main Menu", BLACK, (button_rect.x + 50, button_rect.y + 15))
     return button_rect
+
 
 def show_seeds_screen(screen):
     seed = ""
@@ -51,26 +54,20 @@ def show_seeds_screen(screen):
                     try:
                         if seed != "":
                             cursor = config.sql_connection.cursor(dictionary=True)
+                            cursor.execute("INSERT INTO seeds (seed) VALUES (%s)", (seed,))
+                            config.sql_connection.commit()
+                            print('test')
 
-
-                            # cursor.execute("""SELECT username, level, x, y, seed FROM users WHERE id = %s""", (user_id,))
-                            # user = cursor.fetchone()
-                            # if user.get('username') and user.get('level') and user.get('x') and user.get('y') and user.get('seed'):
-                            #    config.local_username = user.get('username')
-                            #    config.current_level = int(user.get('level'))
-                            #    config.player_x = int(user.get('x'))
-                            #    config.player_y = int(user.get('y'))
-                            #    config.seed = int(user.get('seed'))
-                        
-                    except:
+                    except Exception as e:
+                        print(e)
                         print("Invalid seed!")
-                    
-                    return config.SCREEN_PLAYER_MENU  
+
+                    return config.SCREEN_PLAYER_MENU
 
                 if [seed_rect for _ in seed_rects if seed_rect.collidepoint(event.pos)]:
-                    config.seed = seeds[seed_rects.index(seed_rect)]
+                    config.seed = int(seeds[seed_rects.index(seed_rect)])
                     print("Set seed to", config.seed)
-                    return config.SCREEN_PLAYER_MENU  
+                    return config.SCREEN_PLAYER_MENU
 
             if config.admin:
                 input_box_seed.handle_event(event)
@@ -85,6 +82,7 @@ def show_seeds_screen(screen):
 
         for seed_rect in seed_rects:
             pygame.draw.rect(screen, BLACK, seed_rect)
-            draw_text(screen, seeds[seed_rects.index(seed_rect)], WHITE, (WIDTH // 2 - 50, HEIGHT // 3 + (10 * seed_rects.index(seed_rect))))
-        
+            draw_text(screen, seeds[seed_rects.index(seed_rect)], WHITE,
+                      (WIDTH // 2 - 50, HEIGHT // 3 + (10 * seed_rects.index(seed_rect))))
+
         pygame.display.flip()
